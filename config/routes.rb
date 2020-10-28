bbda0f9a184f87ca233bbee41b1f7d7cfbc2c997
jackie0922youhei <jackie0922youhei@gmail.com>
2020-10-28 13:39:53 +0000

    Being Lost

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
-  
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
-  
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
